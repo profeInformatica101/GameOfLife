959a5e94a0d2e07ba1e2d52e7bca65f617a32a98
profeInfoAtenea <90715116+profeInfoAtenea@users.noreply.github.com>
2022-01-01 21:08:45 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,45 @@
+var rows = 24;
+var cols = 24;
+
+//inicialize
+function initialize(){
+    createTable();
+}
+
+//lay out your board
+function createTable(){
+    var gridContainer = document.getElementById("gridContainer");
+    if(!gridContainer){
+        console.error("Problem: no div for the grid table!");
+    }
+
+
+    var table = document.createElement("table");
+
+    for(var i=0; i < rows; i++){
+        var tr = document.createElement("tr");
+        for(var j=0; j < cols; j++){
+            var cell = document.createElement("td");
+            cell.setAttribute("id", i + "_" + j);
+            cell.setAttribute("class", "dead");
+            cell.onclick = cellClickHandler;
+            tr.appendChild(cell);
+        }
+        table.appendChild(tr);
+    }
+
+    gridContainer.appendChild(table);
+}
+
+
+function cellClickHandler(){
+    var classes = this.getAttribute("class");
+    if(classes.indexOf("live")>-1){
+        this.setAttribute("class", "dead")     
+    }else{
+        this.setAttribute("class", "live")
+    }
+}
+
+//start everything
+window.onload = initialize;
